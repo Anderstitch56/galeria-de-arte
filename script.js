ec4737c6fa0bde0842457fe2e6c5d605793a1e59
Anderstitch <112553484+Anderstitch56@users.noreply.github.com>
2025-08-27 14:32:10 -0300

    primeira versão da galeria - 1

---
+++ script.js
@@ -0,0 +1,15 @@
+function abrirLightbox(img) {
+    const lightbox = document.getElementById("lightbox");
+    const lightboxImg = document.getElementById("lightbox-img");
+    const lightboxCaption = document.getElementById("lightbox-caption");
+
+    lightboxImg.src = img.src;
+    lightboxCaption.textContent = img.nextElementSibling ? img.nextElementSibling.textContent : "";
+    lightbox.classList.add("show");
+}
+
+
+function fecharLightbox() {
+    const lightbox = document.getElementById("lightbox");
+    lightbox.classList.remove("show");
+}
